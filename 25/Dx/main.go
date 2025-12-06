31a01b33444bc1d6d8e0454f79de7c8b869f6deb
kr9sis <mkmw464l@anonaddy.com>
2025-12-06 13:05:39 +0000

    Moved reading of file to main.go for more DRY code

--- 25/Dx/main.go
+++ 25/Dx/main.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
+func Readfile(filename string) *os.File {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	return file
+}
+
 func main() {
 	filename := "puzzle.txt"
-	// filename = "small_puzzle.txt"
+	filename = "small_puzzle.txt"
 	Part1(filename)
 }
--- 25/Dx/p1.go
+++ 25/Dx/p1.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"log"
-	"os"
-)
-
 func Part1(filename string) {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	f := Readfile(filename)
 }
