70102b21056801a64bc346fa8c28535ad0dcd1ba
kr9sis <mkmw464l@anonaddy.com>
2025-12-07 20:07:24 +0000

    Finished D3 P2

--- 25/D3/p2.go
+++ 25/D3/p2.go
@@ -6,11 +6,31 @@ import (
 	"log"
 )
 
+func get_max_digit(line string) int {
+	const win_size = 12
+	const line_len = 100
+	var m [line_len + 1][win_size + 1]int
+
+	for i := range line_len {
+		number := int(line[i] - '0') // Get character at index i as int
+
+		max_single_digit := m[i][1]
+		m[i+1][1] = max(max_single_digit, number)
+
+		for j := 2; j <= win_size; j++ {
+			current_max_combined := m[i][j-1]*10 + number
+			previous_max := m[i][j]
+			m[i+1][j] = max(current_max_combined, previous_max)
+		}
+	}
+
+	return m[line_len][win_size]
+}
+
 func Part2(filename string) {
 	f := ReadFile(filename)
 	defer f.Close()
 
-	const window_size = 12
 	ret := 0
 	line_num := 0
 	scanner := bufio.NewScanner(f)
@@ -19,26 +39,13 @@ func Part2(filename string) {
 		fmt.Println("Line", line_num)
 		line := scanner.Text() // Read one line
 
-		ret += GetJolt(line)
+		line_ret := get_max_digit(line)
+		fmt.Println(line_ret)
+		ret += line_ret
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(ret, "\n", err)
 	}
 
-	fmt.Println(ret)
+	fmt.Println("\nResult:", ret)
 }
-
-/* INFO:
-*  Line 1
-*  987654321111 c
-*  987654321111 c
-*  Line 2
-*  811111111119 c
-*  811111111119 c
-*  Line 3
-*  343423423478 x
-*  4 34234234278 c
-*  Line 4
-*  818181911112 x
-*  888911112111 c
-* */
